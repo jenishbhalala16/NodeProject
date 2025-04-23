a6b961b9b07c1131e6b316fa2997ead34542a89a
Jenish Bhalala <jenishbhalala122@gmail.com>
2025-04-23 15:11:40 +0530

    Vercel added

--- index.js
+++ index.js
@@ -422,6 +422,9 @@ app.delete("/:id", (req,res) => {
 
 })
 
-app.listen(3000, () => { 
-    console.log("Server listening on port 3000");
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => { 
+    console.log("Server listening on port" + port);
 });
